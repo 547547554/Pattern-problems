87258e55b2e5277898721e783e69ea35a9c368dd
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-10-09 23:55:45 +0530

    Add files via upload

---
+++ same-row.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    for (row = 1; row <= 5; row++)
+    {
+        for(col=1;col<=5;col++)
+        {
+            cout<<row<<" ";
+        }
+         cout<<endl;
+    }
+}
+
+//output:-
+// 11111
+// 22222
+// 33333
+// 44444
+// 55555
