91355e6fb1e63744a68a44a69a06b1c10683e033
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-10-10 19:30:55 +0530

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int col,row;
+    for(row=1;row<=5;row++)
+    {
+        for(col=1;col<=5;col++)
+        {
+            cout<<col*col<<" ";
+        }
+           cout<<endl;
+    }
+ 
+}
